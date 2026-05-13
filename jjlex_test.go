626367139f0e947e6ace95db7f7ec0b77c8752fc
rsteube <rsteube@users.noreply.github.com>
2026-05-13 18:34:21 +0200

    added test

--- jjlex_test.go
+++ jjlex_test.go
@@ -4,61 +4,25 @@ import "testing"
 
 func TestSplit(t *testing.T) {
 	for revset, expected := range map[string]CompletionContext{
-		"rev": {
-			Prefix: "rev",
-			Type:   CompletionTypeRevision,
-		},
-		"rev-": {
-			Prefix: "rev-",
-			Type:   CompletionTypeRevision,
-		},
-		"rev&": {
-			Prefix: "rev&",
-			Type:   CompletionTypeRevision,
-		},
-		"rev &": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		"rev & ": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		"parents(": {
-			Type: CompletionTypeFunctionArg,
-		},
-		"parents(a": {
-			Prefix: "a",
-			Type:   CompletionTypeFunctionArg,
-		},
-		"parents(a)": {
-			Prefix: "",
-			Type:   CompletionTypeOperator,
-		},
-		"parents(a)|": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		"parents(a)| ": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		"parents(a) |": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		"parents(a) | ": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
-		":": {
-			Prefix: ":",
-			Type:   CompletionTypeOperator,
-		},
-		"::": {
-			Prefix: "",
-			Type:   CompletionTypeRevision,
-		},
+		"rev":      {Prefix: "rev", Type: CompletionTypeRevision},
+		"rev-":     {Prefix: "rev-", Type: CompletionTypeRevision},
+		"rev&":     {Prefix: "rev&", Type: CompletionTypeRevision},
+		"rev &":    {Prefix: "", Type: CompletionTypeRevision},
+		"rev & ":   {Prefix: "", Type: CompletionTypeRevision},
+		"parents(": {Type: CompletionTypeFunctionArg}, "parents(a": {Prefix: "a", Type: CompletionTypeFunctionArg},
+		"parents(a)":               {Prefix: "", Type: CompletionTypeOperator},
+		"parents(a)|":              {Prefix: "", Type: CompletionTypeRevision},
+		"parents(a)| ":             {Prefix: "", Type: CompletionTypeRevision},
+		"parents(a) |":             {Prefix: "", Type: CompletionTypeRevision},
+		"parents(a) | ":            {Prefix: "", Type: CompletionTypeRevision},
+		":":                        {Prefix: ":", Type: CompletionTypeOperator},
+		"::":                       {Prefix: "", Type: CompletionTypeRevision},
+		"::r":                      {Prefix: "r", Type: CompletionTypeRevision},
+		"(":                        {Prefix: "", Type: CompletionTypeRevision},
+		"(y & z":                   {Prefix: "z", Type: CompletionTypeRevision},
+		"(y & z) ":                 {Prefix: "", Type: CompletionTypeOperator},
+		"heads(::@ & bookmark":     {Prefix: "bookmark", Type: CompletionTypeRevision},
+		"heads(::@ & bookmarks())": {Prefix: "", Type: CompletionTypeOperator},
 	} {
 		t.Run(revset, func(t *testing.T) {
 			actual := Split(revset)
